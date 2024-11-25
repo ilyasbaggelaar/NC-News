3a96c20743d2c342f6350b22c51e0a4d1de28d24
ilyasbaggelaar <ilyas.baggelaar@gmail.com>
2024-11-25 15:45:31 +0000

    Finished task 3

--- db/Controllers/api.controller.js
+++ db/Controllers/api.controller.js
@@ -1,5 +1,17 @@
 const endpoints = require('../../endpoints.json')
+const {readTopics} = require('../models/api.model')
 
 exports.getApiDocu = (req, res) => {
     res.status(200).send({endpoints})
+}
+
+exports.getTopics = (req, res) => {
+        readTopics().then((topics) => {
+            //console.log(topics)
+            res.status(200).send({topics})
+        })
+        .catch((err) => {
+            console.error("Error fetching topics:", err);
+            res.status(500).send({msg: "server error"})
+        })
 }
